deb184ca2917d07c0af43532b089275e69f5ff4b
andisis <andisis92@gmail.com>
2018-02-06 12:22:09 +0700

    Create rajaongkir.php

---
+++ config/rajaongkir.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * @author Andi Siswanto <andisis92@gmail.com>
+ * RajaOngkir API Key
+ * Silakan daftar akun di RajaOngkir.com untuk mendapatkan API Key
+ * http://rajaongkir.com/akun/daftar
+ */
+$config['rajaongkir_api_key'] = "api_key_kamu";
+
+/**
+ * RajaOngkir account type: starter / basic / pro
+ * http://rajaongkir.com/dokumentasi#akun-ringkasan
+ * 
+ */
+$config['rajaongkir_account_type'] = "tipe_akun_kamu";
